6016d06b5111f179f40b72498741692d5a07eafb
martha ochoa <marthaeochoa98@gmail.com>
2023-02-18 18:35:38 -0600

    more table styling

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -6,6 +6,11 @@ function Playlist ({songs}) {
     return (
         <section>
             <table className="song-table">
+            <tr>
+                    <td><h3>Title</h3></td>
+                    <td><h3>Artist</h3></td>
+                    <td><h3>Genre</h3></td>
+             </tr>
             {
                 songs.map(song => {
                     return (
