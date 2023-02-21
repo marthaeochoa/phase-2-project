33a5a5344f084f66560349fedbdc62586aa4c5e0
martha ochoa <marthaeochoa98@gmail.com>
2023-02-20 19:51:52 -0600

    add button

--- src/components/GenreFilter.js
+++ src/components/GenreFilter.js
@@ -1,8 +1,14 @@
 import React from "react"
 
 function GenreFilter () {
+
+function handleOnClick(){
+
+}
+
     return (
         <div className="filter">
+            <button>All</button>
             <button>Pop</button>
             <button>R&B</button>
             <button>Alternative</button>
